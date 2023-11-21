e545d3b31297ff002d08176e3ca32055f731c04a
LuisSebastian28 <sabestech@gmail.com>
2023-11-20 21:38:58 -0400

    arreglo

--- src/calendar.js
+++ src/calendar.js
@@ -2,19 +2,15 @@ import React from "react";
 import Calendar from "@ericz1803/react-google-calendar";
 import { css } from "@emotion/react";
 
-// put your google calendar api key here
 const API_KEY = "AIzaSyAluLKMMt6FEryHw5H2UzpLk9yPtMGSkGg";
 
-// replace calendar id with one you want to test
 let calendars = [{ calendarId: "09opmkrjova8h5k5k46fedmo88@group.calendar.google.com" }];
 
 let styles = {
-  // you can use object styles
   calendar: {
-    borderWidth: "3px" // make outer edge of calendar thicker
+    borderWidth: "3px"
   },
 
-  // you can also use emotion's string styles (remember to add the line 'import { css } from "@emotion/react";')
   today: css`
     /* highlight today by making the text red and giving it a red border */
     color: red;
